46bd2852adb3e42eec3d654d07dfc241ca98ecdd
Stephan Constantial <Sconstantial@yahoo.com>
2018-09-29 15:44:23 +0100

    map helper

--- models/album.rb
+++ models/album.rb
@@ -1,44 +1,49 @@
 require_relative('../db/sql_runner.rb'
-class Album
+  class Album
 
-  attr_accessor :title, :genre, :stock
-  attr_reader :id
+    attr_accessor :title, :genre, :stock
+    attr_reader :id
 
-  def initialize (options)
-    @title = options['title']
-    @genre = options['genre']
-    @stock = options['stock'].to_i
-    @id = options['id'].to_i
+    def initialize (options)
+      @title = options['title']
+      @genre = options['genre']
+      @stock = options['stock'].to_i
+      @id = options['id'].to_i
+    end
+
+    def save()
+      sql = "INSERT INTO albums(title, genre, stock) VALUES($1, $2, $3)RETURNING id"
+      values = [@title, @genre, @stock]
+      album = SqlRunner.run(sql, values)
+      @id = album[0]['id'].to_i
+    end
+
+    def update()
+      sql = "UPDATE albums SET (title, genre, stock) = ($1, $2, $3) WHERE id = $3"
+      values = [@title, @genre, @stock]
+      album = SqlRunner.run(sql, values)
+      @id = album['id'].to_i
+    end
+
+    def delete()
+      sql = "DELETE * FROM album WHERE id = $1"
+      values =[@id]
+      SqlRunner.run(sql, values)
+    end
+
+    def self.delete_all
+      sql ="DELETE FROM albums"
+      SqlRunner.run(sql)
+    end
+
+    def self.all()
+      sql = "SELECT * FROM albums"
+      album_data = SqlRunner.run(sql)
+      albums = map_items(album_data)
+      return albums
+    end
+
+    def self.map_items(album_data)
+      return album_data.map {|album| Album.new(album)}
+    end
   end
-
-  def save()
-    sql = "INSERT INTO albums(title, genre, stock) VALUES($1, $2, $3)RETURNING id"
-    values = [@title, @genre, @stock]
-    album = SqlRunner.run(sql, values)
-    @id = album[0]['id'].to_i
-  end
-
-  def update()
-    sql = "UPDATE albums SET (title, genre, stock) = ($1, $2, $3) WHERE id = $3"
-    values = [@title, @genre, @stock]
-    album = SqlRunner.run(sql, values)
-    @id = album['id'].to_i
-  end
-
-  def delete()
-    sql = "DELETE * FROM album WHERE id = $1"
-    values =[@id]
-    SqlRunner.run(sql, values)
-  end
-
-  def self.delete_all
-    sql ="DELETE FROM albums"
-    SqlRunner.run(sql)
-  end
-
-  def self.all()
-    sql = "SELECT * FROM albums"
-    album_data = SqlRunner.run(sql)
-
-  end
-end
